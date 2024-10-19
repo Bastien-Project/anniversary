7df5b99dc62c29cfe88cc57556f80b374e864cc4
Bastien Thomas <46076642+Bastien-Project@users.noreply.github.com>
2024-10-19 18:40:07 +0200

    v2

--- index.php
+++ index.php
@@ -5,125 +5,124 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta charset="UTF-8">
   <link rel="stylesheet" type="text/css" href="style.css">
+  <script src="script.js" defer></script>
 </head>
 
 <body>
-  <?php 
-  if (!empty($_GET['birthday'])) {
-    $date = new DateTime($_GET['birthday']);
-    $year = $date->format('Y');
-    $month = $date->format('m');
-    $day = $date->format('d');
-    $hour = $date->format('H');
-    $minutes = $date->format('i');
-    $seconds = $date->format('s');
-
-    echo '<pre>';
-    var_dump($year, $month, $day, $hour, $minutes, $seconds);
-
+  <?php
+  if (!empty($_POST['birthday'])) {
+    $birthday = $_POST['birthday'];
+    $date = new DateTime($birthday);
     $now = new DateTime();
     $interval = $now->diff($date);
-    var_dump($interval);
-    echo '</pre>';
-  }
+  } else {
   ?>
-  
-  <!-- formulaire entrer sa date de naissance -->
-  <form>
-    <label for="year">Année de naissance</label>
-    <select name="year" id="year" required>
-      <?php
-      $currentYear = intval(date('Y'));
-      for ($i = $currentYear; $i >= 1900; $i--) {
-        $selected = (isset($year) && $i == $year) ? 'selected' : '';
-        echo "<option value='$i' $selected>$i</option>";
-      }
-      ?>
-    </select>
-    <label for="month">Mois de naissance</label>
-    <select name="month" id="month" required>
-      <?php
-      for ($i = 1; $i <= 12; $i++) {
-        $selected = (isset($month) && $i == $month) ? 'selected' : '';
-        echo "<option value='$i' $selected>$i</option>";
-      }
-      ?>
-    </select>
-    <label for="day">Jour de naissance</label>
-    <select name="day" id="day" required>
-      <?php
-      for ($i = 1; $i <= 31; $i++) {
-        $selected = (isset($day) && $i == $day) ? 'selected' : '';
-        echo "<option value='$i' $selected>$i</option>";
-      }
-      ?>
-    </select>
-    <label for="hour">Heure de naissance</label>
-    <select name="hour" id="hour">
-      <?php
-      for ($i = 0; $i <= 23; $i++) {
-        $selected = (isset($hour) ? ($i == $hour) : ($i == 12)) ? 'selected' : '';
-        echo "<option value='$i' $selected>$i</option>";
-      }
-      ?>
-    </select>
-    <label for="minute">Minute de naissance</label>
-    <select name="minute" id="minute">
-      <?php
-      for ($i = 0; $i <= 59; $i++) {
-        $selected = (isset($minutes) && $i == $minutes) ? 'selected' : '';
-        echo "<option value='$i' $selected>$i</option>";
-      }
-      ?>
-    </select>
-    <label for="second">Seconde de naissance</label>
-    <select name="second" id="second">
-      <?php
-      for ($i = 0; $i <= 59; $i++) {
-        $selected = (isset($seconds) && $i == $seconds) ? 'selected' : '';
-        echo "<option value='$i' $selected>$i</option>";
-      }
-      ?>
-    </select>
-    <input type="submit" value="Valider">
-  </form>
 
-  <?php
-  // Vérifier si les paramètres GET sont définis et valides
-  if (!empty($_GET['birthday'])) {
-  ?>
+    <form id="birthdayForm" method="POST">
+      <p>Entrez votre date de naissance :</p>
+      <div>
+        <label for="year">Année de naissance :&nbsp</label>
+        <select name="year" id="year" required>
+          <?php
+          $currentYear = intval(date('Y'));
+          for ($i = $currentYear; $i >= 1900; $i--) {
+            $selected = (isset($year) && $i == $year) ? 'selected' : '';
+            echo "<option value='$i' $selected>$i</option>";
+          }
+          ?>
+        </select>
+      </div>
+      <div>
+        <label for="month">Mois de naissance :&nbsp</label>
+        <select name="month" id="month" required>
+          <?php
+          for ($i = 1; $i <= 12; $i++) {
+            $selected = (isset($month) && $i == $month) ? 'selected' : '';
+            echo "<option value='$i' $selected>$i</option>";
+          }
+          ?>
+        </select>
+      </div>
+      <div>
+        <label for="day">Jour de naissance :&nbsp</label>
+        <select name="day" id="day" required>
+          <?php
+          for ($i = 1; $i <= 31; $i++) {
+            $selected = (isset($day) && $i == $day) ? 'selected' : '';
+            echo "<option value='$i' $selected>$i</option>";
+          }
+          ?>
+        </select>
+      </div>
+      <div>
+        <label for="hour">Heure de naissance :&nbsp</label>
+        <select name="hour" id="hour">
+          <?php
+          for ($i = 0; $i <= 23; $i++) {
+            $selected = (isset($hour) && $i == $hour) ? 'selected' : '';
+            echo "<option value='$i' $selected>$i</option>";
+          }
+          ?>
+        </select>
+      </div>
+      <div>
+        <label for="minute">Minute de naissance :&nbsp</label>
+        <select name="minute" id="minute">
+          <?php
+          for ($i = 0; $i <= 59; $i++) {
+            $selected = (isset($minutes) && $i == $minutes) ? 'selected' : '';
+            echo "<option value='$i' $selected>$i</option>";
+          }
+          ?>
+        </select>
+      </div>
+      <div>
+        <label for="second">Seconde de naissance :&nbsp</label>
+        <select name="second" id="second">
+          <?php
+          for ($i = 0; $i <= 59; $i++) {
+            $selected = (isset($seconds) && $i == $seconds) ? 'selected' : '';
+            echo "<option value='$i' $selected>$i</option>";
+          }
+          ?>
+        </select>
+      </div>
+
+      <input type="hidden" name="birthday" id="birthday">
+
+      <input type="submit" value="Valider">
+    </form>
+
+  <?php }
+  if (!empty($_POST['birthday'])) { ?>
     <div class="container countdown">
-      <div id="demo"></div>
-      <div class="countdowncontainer">
-        <div id="years" class="clock_bg">
-          <div class="clock_counter"></div>
-          <div class="clock_label">années</div>
-        </div>
-        <div id="weeks" class="clock_bg">
-          <div class="clock_counter"></div>
-          <div class="clock_label">semaines</div>
-        </div>
-        <div id="days" class="clock_bg">
-          <div class="clock_counter"></div>
-          <div class="clock_label">jours</div>
-        </div>
-        <div id="hours" class="clock_bg">
-          <div class="clock_counter"></div>
-          <div class="clock_label">heures</div>
-        </div>
-        <div id="minutes" class="clock_bg">
-          <div class="clock_counter"></div>
-          <div class="clock_label">minutes</div>
-        </div>
-        <div id="seconds" class="clock_bg">
-          <div class="clock_counter"></div>
-          <div class="clock_label">secondes</div>
-        </div>
+      <div id="years" class="clock_bg">
+        <div class="clock_counter"><?php echo $interval->y; ?></div>
+        <div class="clock_label">années</div>
+      </div>
+      <div id="months" class="clock_bg">
+        <div class="clock_counter"><?php echo $interval->m; ?></div>
+        <div class="clock_label">mois</div>
+      </div>
+      <div id="days" class="clock_bg">
+        <div class="clock_counter"><?php echo $interval->d; ?></div>
+        <div class="clock_label">jours</div>
+      </div>
+      <div id="hours" class="clock_bg">
+        <div class="clock_counter"><?php echo $interval->h; ?></div>
+        <div class="clock_label">heures</div>
+      </div>
+      <div id="minutes" class="clock_bg">
+        <div class="clock_counter"><?php echo $interval->i; ?></div>
+        <div class="clock_label">minutes</div>
+      </div>
+      <div id="seconds" class="clock_bg">
+        <div class="clock_counter"><?php echo $interval->s; ?></div>
+        <div class="clock_label">secondes</div>
       </div>
     </div>
   <?php } ?>
 
-  <script type="text/javascript" src="script.js"></script>
 </body>
 
 </html>
--- script.js
+++ script.js
@@ -1,49 +1,47 @@
-function minTwoDigits(n) {
-    return (n < 10 ? '0' : '') + n;
-}
-
-// Récupérer les valeurs du formulaire et mettre à jour l'URL
-const form = document.querySelector('form');
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const year = document.querySelector('#year').value;
-    const month = document.querySelector('#month').value;
-    const day = document.querySelector('#day').value;
-    const hour = document.querySelector('#hour').value;
-    const minute = document.querySelector('#minute').value;
-    const second = document.querySelector('#second').value;
-    const birthday = `${year}-${minTwoDigits(month)}-${minTwoDigits(day)}T${minTwoDigits(hour)}:${minTwoDigits(minute)}:${minTwoDigits(second)}`;
-    console.log(birthday);
-    const url = `index.php?birthday=${birthday}`;
-    window.location.href = url;
-});
-
-// Mettre à jour le compteur chaque seconde
-setInterval(function () {
-    const urlParams = new URLSearchParams(window.location.search);
-    const birthdayParam = urlParams.get('birthday');
-
-    if (!birthdayParam) return;
-
-    console.log(birthdayParam);
-
-    const annivDate = new Date(birthdayParam).getTime();
-    const now = new Date().getTime();
-    const distance = Math.trunc((now - annivDate) /1000);
-
-    const days = Math.floor(distance / 86400);
-    const weeks = Math.floor(days / 7);
-    const years = Math.floor(weeks / 52);
-    const weeksRemainder = weeks % 52;
-    const daysRemainder = days % 7;
-    const hours = Math.floor((distance % (86400)) / 3600);
-    const minutes = Math.floor((distance % 3600) / 60);
-    const seconds = Math.floor((distance % 60));
-
-    document.querySelector("#years .clock_counter").innerText = minTwoDigits(years);
-    document.querySelector("#weeks .clock_counter").innerText = minTwoDigits(weeksRemainder);
-    document.querySelector("#days .clock_counter").innerText = minTwoDigits(daysRemainder);
-    document.querySelector("#hours .clock_counter").innerText = minTwoDigits(hours);
-    document.querySelector("#minutes .clock_counter").innerText = minTwoDigits(minutes);
-    document.querySelector("#seconds .clock_counter").innerText = minTwoDigits(seconds);
-}, 1000);
+function updateCountdown(birthday) {
+    const birthDate = new Date(birthday);
+    setInterval(function () {
+      const now = new Date();
+      const diff = new Date(now - birthDate);
+  
+      const years = diff.getUTCFullYear() - 1970;
+      const months = diff.getUTCMonth();
+      const days = diff.getUTCDate() - 1;
+      const hours = diff.getUTCHours();
+      const minutes = diff.getUTCMinutes();
+      const seconds = diff.getUTCSeconds();
+  
+      document.querySelector('#years .clock_counter').innerText = years;
+      document.querySelector('#months .clock_counter').innerText = months;
+      document.querySelector('#days .clock_counter').innerText = days;
+      document.querySelector('#hours .clock_counter').innerText = hours;
+      document.querySelector('#minutes .clock_counter').innerText = minutes;
+      document.querySelector('#seconds .clock_counter').innerText = seconds;
+    }, 1000);
+  }
+  
+  document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('birthdayForm').addEventListener('submit', function (e) {
+      e.preventDefault();
+  
+      var year = document.getElementById('year').value;
+      var month = document.getElementById('month').value.padStart(2, '0');
+      var day = document.getElementById('day').value.padStart(2, '0');
+      var hour = document.getElementById('hour').value.padStart(2, '0');
+      var minute = document.getElementById('minute').value.padStart(2, '0');
+      var second = document.getElementById('second').value.padStart(2, '0');
+  
+      // Format ISO 8601
+      var birthday = `${year}-${month}-${day}T${hour}:${minute}:${second}`;
+      document.getElementById('birthday').value = birthday;
+  
+      this.submit();
+      updateCountdown(birthday);
+    });
+  
+    const birthdayInput = document.getElementById('birthday').value;
+    if (birthdayInput) {
+      updateCountdown(birthdayInput);
+    }
+  });
+  
